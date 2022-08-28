23ec16fc9a84a126371af378d6e900dc0e29e295
briangor <briangor13@gmail.com>
2022-08-28 08:40:09 +0300

    add client

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import { books } from './db.js';
 
 const app = express();
+app.use(cors());
 
 const hostname = 'briangor.xyz';
 const port = 3000;
 
-app.set('views', '')
+
 app.get('/', (req, res) => {
     res.render('index', { title: 'Hey', message: 'Hello there!' })
     res.send('REST API with Node.js!');
 });
 app.get('/books', books);
-
 
 //app.get('/books/:id', (req, res) => {});
 
