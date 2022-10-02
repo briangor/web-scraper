768b4a69cbe9c5a1d754b64ffb23d0f07dc75318
briangor <briangor13@gmail.com>
2022-10-02 21:20:13 +0300

    bugfix script.js

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -47,52 +47,30 @@ let getBooks = async () => {
                         price.innerHTML = book.price;
                         image.src = book.image;
 
-                        //console.log(card)
-                        booksData.forEach(book => {
-                            //display(book.title, book.price);
-                            let card = document.createElement('div');
-                            let title = document.createElement('h5');
-                            let author = document.createElement('p');
-                            let price = document.createElement('p');
-                            let image = document.createElement('img');
-                            card.append(image, title, author, price);
-                            card.classList.add('card', 'col-', 'col-sm-4', 'col-md-3');
-                            title.classList.add('card-title', 'fw-bold');
-                            author.classList.add('card-author');
-                            price.classList.add('card-price', 'fw-bold', 'text-success');
-                            image.classList.add('card-image');
-                            title.innerHTML = book.title;
-                            author.innerHTML = book.author;
-                            price.innerHTML = book.price;
-                            image.src = book.image;
-
-                            //console.log(card)
-
-                            card_row.append(card)
-                        })
                         placeholders.remove();
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
+                    }) 
+                })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 // Invoke the function to fetch data
 getBooks();
 
-            btn?.addEventListener('click', getBooks);
+btn?.addEventListener('click', getBooks);
 
-            let getSearchInput = () => {
-                let keyword = search_input.value;
-                //console.log(booksData);
-                booksData.forEach(book => {
-                    console.log(book.title);
-                    if (book.title == keyword) {
-                        console.log('Found the book!');
-                        console.table(book.title, book.author, book.price);
-                    }
-                })
+let getSearchInput = () => {
+    let keyword = search_input.value;
+    //console.log(booksData);
+    booksData.forEach(book => {
+        console.log(book.title);
+        if (book.title == keyword) {
+            console.log('Found the book!');
+            console.table(book.title, book.author, book.price);
+        }
+    })
 
-            }
-
-            search_btn?.addEventListener('click', getSearchInput);
-
+}
+search_btn?.addEventListener('click', getSearchInput);
